58b267ea2abf6047b2496419efbfb39c7d30521f
Muhammad Abdullah Khan <162979409+abdullah-2025@users.noreply.github.com>
2025-02-25 10:40:11 +0000

    variables & data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3);
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value      "null is a object in javascript"
+// undefined => 
+// Symbol
+
+console.log(typeof Boolean)
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountID = 2312280
+let accountEmail = "abdullah.devolp@gmail.com"
+var accountPassword = "12345"
+let accountCity = "Islamabad"
+let accountState;
+let accountBalance = null
+
+// accountID = 222 // Not Allowed 
+
+console.log(accountID);
+console.table([accountID, accountEmail,accountPassword, accountCity, accountState, accountBalance])
